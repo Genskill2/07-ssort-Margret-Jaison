f48fb20ee3ce8d99f8b6de0b06575104996c6adc
Margret-Jaison <77883030+Margret-Jaison@users.noreply.github.com>
2021-07-01 08:16:33 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,24 +1,26 @@
   
-#include<stdio.h>
-#include<cs50.h>
-void swap_max(int arr[], int l, int n)
-{ int max=arr[n],temp;
-  int position=n,i;
-  for(i=n;i<l;i++)
-  { if(arr[i]>max)
-        {max=arr[i];
-         position=i;
-        }
-   }
-  temp=arr[position];
-  arr[position]=arr[n];
-  arr[n]=temp;
- } 
- 
- 
- void ssort(int arr[], int l)
- { for(int i=0;i<l;i++)
-     { swap_max(arr,l,i);
-     }
   
- }
+#include<stdio.h>
+
+void ssort(int arr[],int l)
+{
+    for(int i=0;i<l;i++)
+    { swap_max(arr,l,i);
+    }
+}
+
+void swap_max(int arr[],int l,int n)
+{
+    int j,t;
+    int temp;
+
+    for(j=t=n;j<l;j++)
+    {
+        if(arr[j] > arr[t])
+        {   t = j;
+        }
+    }
+    temp = arr[n];
+    arr[n] = arr[t];
+    arr[t] = temp;
+}
